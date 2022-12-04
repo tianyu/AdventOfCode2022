1369214342ba43de48f4bcc7b387ad4d9b2c5103
Tianyu Zhu <tianyu.zhu@tamr.com>
2022-12-04 09:32:42 -0500

    Day 4

---
+++ src/main/kotlin/day4.kt
@@ -0,0 +1,41 @@
+private fun main() {
+  part1()
+  println()
+  part2()
+}
+
+private fun part1() {
+  println("-- Part 1 --")
+  val numberOfFullyRedundantRanges = sectionAssignmentPairs().count { (left, right) ->
+    left in right || right in left
+  }
+  println("The number of fully redundant ranges is: $numberOfFullyRedundantRanges")
+}
+
+private fun part2() {
+  println("--- Part 2 ---")
+  val numberOfOverlappingRanges = sectionAssignmentPairs().count { (left, right) ->
+    left overlapsWith right
+  }
+  println("The number of overlapping ranges is: $numberOfOverlappingRanges")
+}
+
+private operator fun IntRange.contains(that: IntRange): Boolean =
+  first <= that.first && last >= that.last
+
+private infix fun IntRange.overlapsWith(that: IntRange): Boolean =
+  first in that || last in that || that in this
+
+private fun sectionAssignmentPairs() = sequence {
+  withInputLines("day4.txt") {
+    forEach { line ->
+      val (left, right) = line.split(',')
+      yield(left.asAssignment() to right.asAssignment())
+    }
+  }
+}
+
+private fun String.asAssignment(): IntRange {
+  val (lo, hi) = split('-')
+  return lo.toInt()..hi.toInt()
+}
